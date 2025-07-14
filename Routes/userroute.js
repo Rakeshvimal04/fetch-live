114bb37406da8345658bc41925fdcaaa830e342e
Rakesh Vimal <vimalrakesh2003@gmail.com>
2025-07-14 12:18:25 +0530

    first commit

--- Routes/userroute.js
+++ Routes/userroute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json([{ name: "Rakesh" }, { ROle: "Fullstack Dev" }]);
+  res.json([{ name: "Rakesh" }, { role: "Fullstack Dev" }]);
 });
 
 module.exports = router;
